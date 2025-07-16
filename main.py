8ede8cb51c5a46d9d485669ac7672d2e7eeddd99
Aaron Pope <aaronpope007@gmail.com>
2025-07-16 09:45:50 -0500

    added ability for users to enter their own prompts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,10 +8,14 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    ai_response = client.models.generate_content(model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(ai_response.text)
-    print(f"Prompt tokens: {ai_response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {ai_response.usage_metadata.candidates_token_count}")
+    if len(sys.argv) == 1:
+        print("You must provide a prompt in the form of a string")
+        sys.exit(1)
+    else:
+        ai_response = client.models.generate_content(model='gemini-2.0-flash-001', contents=sys.argv[1])
+        print(ai_response.text)
+        print(f"Prompt tokens: {ai_response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {ai_response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
