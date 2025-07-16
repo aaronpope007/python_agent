74af9ee60c56a19a642a2652423635e0bf3799d0
Aaron Pope <aaronpope007@gmail.com>
2025-07-16 16:24:21 -0500

    changed up some of the main logic

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-import os
 import sys
-from dotenv import load_dotenv
+import os
 from google import genai
+from dotenv import load_dotenv
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
 
 def main():
-    if len(sys.argv) == 1:
-        print("You must provide a prompt in the form of a string")
+    load_dotenv()
+
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
-    else:
-        ai_response = client.models.generate_content(model='gemini-2.0-flash-001', contents=sys.argv[1])
-        print(ai_response.text)
-        print(f"Prompt tokens: {ai_response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {ai_response.usage_metadata.candidates_token_count}")
+    user_prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=user_prompt,
+    )
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
