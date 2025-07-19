908d44aaf9db8ee5dea4d50acda7c19eb21e0b69
Aaron Pope <aaronpope007@gmail.com>
2025-07-18 21:03:38 -0500

    Finished read file functionality

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-print(f"Result for current directory:\n{get_files_info("calculator", ".")}")
-print(f"Result for 'pkg' directory:\n{get_files_info("calculator", "pkg")}")
-print(f"Result for '/bin' directory:\n{get_files_info("calculator", "/bin")}")
-print(f"Result for '../' directory:\n{get_files_info("calculator", "../")}")
+print(f'Result for current file: {get_file_content("calculator", "main.py")}')
+print(f'Result for current file: {get_file_content("calculator", "pkg/calculator.py")}')
+print(f'Result for current file: {get_file_content("calculator", "/bin/cat")}')
+print(f'Result for current file: {get_file_content("calculator", "pkg/does_not_exist.py")}')
