962d9b4bfb393001f03db2b8ca40531a6896cbc0
Aaron Pope <aaronpope007@gmail.com>
2025-07-19 09:56:21 -0500

    added logic to run python files and updated test cases to verify

---
+++ functions/run_python_file.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=[]):
+    full_file_path = os.path.join(working_directory, file_path)
+
+    working_directory_real_path = os.path.realpath(working_directory)
+
+    if not working_directory_real_path.endswith(os.path.sep):
+        working_directory_real_path += os.path.sep
+
+    file_path_real_path = os.path.realpath(full_file_path)
+    file_path_in_working_dir = file_path_real_path.startswith(working_directory_real_path)
+
+    if file_path_in_working_dir == False:
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.exists(full_file_path):
+        return f'Error: File "{file_path}" not found.'
+
+    if not full_file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+
+
+    command = ["python", os.path.basename(file_path)] + args
+
+
+
+    try:
+        completed_process = subprocess.run(
+            command,                                      # The command and its arguments as a list
+            capture_output=True,                          # This captures both stdout and stderr
+            text=True,                                    # This returns output as a string instead of bytes
+            cwd=working_directory_real_path,                        # Run command in a particular working directory
+            timeout=30                                    # Abort if still running after 30 seconds
+        )
+        if not completed_process.stdout and not completed_process.stderr:
+            return "No output produced."
+        result = f"STDOUT:{completed_process.stdout.rstrip()}\nSTDERR:{completed_process.stderr.rstrip()}"
+        if completed_process.returncode != 0:
+            result += f"\nProcess exited with code {completed_process.returncode}"
+        return result
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
-from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
-print(f'Result for current file: {write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")}')
-print(f'Result for current file: {write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")}')
-print(f'Result for current file: {write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-}')
+print(f'Result for current file: {run_python_file("calculator", "main.py")}')
+print(f'Result for current file: {run_python_file("calculator", "main.py", ["3 + 5"])}')
+print(f'Result for current file: {run_python_file("calculator", "tests.py")}')
+print(f'Result for current file: {run_python_file("calculator", "../main.py")}')
+print(f'Result for current file: {run_python_file("calculator", "nonexistent.py")}')
