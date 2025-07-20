e11b7a48d7531802c594016c39f8c5c2d264497e
Aaron Pope <aaronpope007@gmail.com>
2025-07-20 08:35:38 -0500

    Added schemas for ability of ai agent to use all functions in the project

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,5 +1,20 @@
 import os
 import subprocess
+from google.genai import types
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Runs python file.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the Python file to execute, relative to the working directory.",
+            ),
+        },
+    ),
+)
 
 def run_python_file(working_directory, file_path, args=[]):
     full_file_path = os.path.join(working_directory, file_path)
--- functions/write_file.py
+++ functions/write_file.py
@@ -1,4 +1,23 @@
 import os
+from google.genai import types
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes files.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to write, relative to the working directory.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to write into the file.",
+            ),
+        },
+    ),
+)
 
 def write_file(working_directory, file_path, content):
 
